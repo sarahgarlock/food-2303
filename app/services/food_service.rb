5c6381c261a7f8db4898b0e2b0176ff040e6753b
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-21 09:26:49 -0600

    add food service

---
+++ app/services/food_service.rb
@@ -0,0 +1,16 @@
+class FoodService
+  def search_food(food)
+    get_url("search?query=#{food}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1/foods') do |faraday|
+      faraday.params['api_key'] = ENV['api_key']
+    end
+  end
+end
