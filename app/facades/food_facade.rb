cf87037b2561343223662930ec733058357d6be2
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-21 09:47:43 -0600

    update method to search_food

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,11 +1,9 @@
 class FoodFacade
-  def FoodService
-    FoodService.new
-  end
 
-  def foods
-    json = service.search_food(food)
-    json[:foods].map do |food_data|
+  def search_food(food)
+    service = FoodService.new
+    request = service.search_food(food)
+    @foods = request[:foods].map do |food_data|
       Food.new(food_data)
     end
   end
